cd5d321f13d619e3d09ee49ba5363a167f3d7dea
Paulo Henrique <paulo.hsilvavieira@gmail.com>
2020-04-28 18:34:54 -0300

    Implementar  typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TabulationsModule } from './tabulations/tabulations.module';
 import { RecordingsModule } from './recordings/recordings.module';
 import { MatchingsModule } from './matchings/matchings.module';
+import path = require('path');
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: path.join(__dirname, '../database.db'),
+      entities: ['dist/**/*.entity.js'],
+    }),
     TabulationsModule,
     RecordingsModule,
     MatchingsModule,
